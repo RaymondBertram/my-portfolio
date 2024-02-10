109cd2489abdaa0aa4e25e83702caa7c5e11387d
Raymond Bertram <raymond.bertram@gmx.de>
2024-02-10 15:32:06 +0100

    initial fundation

---
+++ src/app/fonts.ts
@@ -0,0 +1,8 @@
+import { PT_Sans } from "next/font/google";
+
+export const pt_sans = PT_Sans({
+  style: ["normal", "italic"],
+  weight: "400",
+  display: "swap",
+  subsets: ["cyrillic-ext", "cyrillic", "latin-ext", "latin"],
+});
